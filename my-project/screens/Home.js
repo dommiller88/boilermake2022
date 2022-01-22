161cae11d3dc7aa26ce5717c5cf752eda7b0a3fa
Ashton <90576442+agstatz@users.noreply.github.com>
2022-01-22 15:29:00 -0500

    Added Map Styling

--- my-project/screens/Home.js
+++ my-project/screens/Home.js
@@ -1,6 +1,7 @@
 import { Box, Select, Button, Center, VStack } from 'native-base';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Image, Dimensions } from 'react-native';
+import { Link } from "react-router-native";
 const { width, height } = Dimensions.get('window');
 
 const stylesLogo = StyleSheet.create({
@@ -14,6 +15,10 @@ const stylesLogo = StyleSheet.create({
 export default function Home() {
     let [university, setUniversity] = useState("");
     let [disabled, setDisabled] = useState(true);
+    let doRedirect = false;
+
+    // Allow button to be used only when
+    // the user has selected a university
     useEffect(() => {
       console.log(university);
       if (university != "") {
@@ -21,7 +26,43 @@ export default function Home() {
       }
     }, [university])
 
+    return (
+        <Center flex={1} px="3">
+            <Box
+            maxW="lg"
+            h="85%"
+            rounded="lg"
+            overflow="hidden"
+            borderColor="coolGray.200"
+            shadow={3}
+            borderWidth="1"
+            _dark={{
+                borderColor: "coolGray.600",
+                backgroundColor: "gray.700",
+            }}
+            _light={{
+                backgroundColor: "gray.50",
+            }}
+            >
+                <VStack space={"lg"} alignItems="center">
+                    <Image
+                        style={stylesLogo.logo}
+                        resizeMode='contain'
+                        source={require('../assets/openspace.png')}
+                    />
+                    <Box _text={{fontSize: "md", textAlign: "center"}} m="4">Use openspace to find the best route to your classes and open study spaces for when you aren't in lecture!</Box>
+                    <Select
+                        selectedValue={university}
+                        width="100%"
+                        size="xl"
+                        minWidth="200"
+                        accessibilityLabel="Choose University"
+                        placeholder="Choose University"
+                        mt={1}
+                        onValueChange={(itemValue) => setUniversity(itemValue)}
+                    >
 
+<<<<<<< Updated upstream
   return (
     <Center flex={1} px="3">
         <Box
@@ -92,3 +133,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+=======
+                        <Select.Item label="Purdue University" value="pu" />
+                        <Select.Item label="Indiana University" value="iu" />
+                        <Select.Item label="Michigan University" value="mu" />
+                        <Select.Item label="University of Illinois" value="uiuc" />
+                        <Select.Item label="Harvard University" value="hu" />
+                    </Select>
+                        <Link to="/map">
+                        <Button 
+                            size="lg"
+                            bgColor="dark.100"
+                            _hover={{
+                                bg: 'dark.200'
+                            }}
+                            _pressed={{
+                                bg: 'dark.200',
+                                borderRadius: '4'
+                            }}
+                            isDisabled={disabled}
+                            >Enter Courses</Button>
+                        </Link>
+                </VStack>
+            </Box>
+        </Center>
+    );
+}
+>>>>>>> Stashed changes
