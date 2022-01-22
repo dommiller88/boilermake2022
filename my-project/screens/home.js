1ab8809637c02774a7183a690940b6eaa90843f9
Caleb <ahn99@purdue.edu>
2022-01-22 01:02:19 -0500

    fixing logo

--- my-project/screens/home.js
+++ my-project/screens/home.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+const win = Dimensions.get('window');
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Open up yo butt to start working on your app!</Text>
+      <Image
+        style={{width: win.width/2, height: win.width/2 }}
+        source={require('../assets/openspace.png')}
+      />
       <StatusBar style="auto" />
     </View>
   );
