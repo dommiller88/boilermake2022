a26e90da141b6a995ca207907020cfb4ca021dc3
Caleb <ahn99@purdue.edu>
2022-01-22 01:26:17 -0500

    logo IS CENTERED SON

--- my-project/screens/Home.js
+++ my-project/screens/Home.js
@@ -2,14 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
+const stylesLogo = StyleSheet.create({
+    container: {
+      paddingTop: 0,
+      paddingBottom: 50,
+    },
+    logo: {
+      width: width/1.2,
+      height: height/1,
+      bottom: height/5,
+    },
+  });
+
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up yo butt to start working on your app!</Text>
       <Image
-        style={{width: 100, height: 100 }}
+        style={stylesLogo.logo}
+        resizeMode='contain'
         source={require('../assets/openspace.png')}
       />
+      <Text>Open up yo butt to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
