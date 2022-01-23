d30edeb9f493b1a87262a725300c8533d0331f02
Shirom <agstatz@gmail.com>
2022-01-23 00:53:35 -0500

    fixed merge conflict errors

--- my-project/components/index.js
+++ my-project/components/index.js
@@ -4,4 +4,3 @@ export { default as CourseInfoEntry } from './CourseInfoEntry.js';
 export { default as CourseSelectModal } from './CourseSelectModal.js'
 export { default as DayOfWeekCheck } from './DayOfWeekCheck.js'
 export { default as MapInfo } from './MapInfo.js';
-export { default as CourseSelectModal } from './CourseSelectModal.js'
